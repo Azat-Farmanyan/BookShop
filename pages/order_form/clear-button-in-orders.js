5b894d0f859450428afeff384d8f22611f8a74f5
Azat-Farmanyan <azat.farmanyan2@mail.ru>
2022-05-14 16:54:33 +0400

    createbinputs for orderForm

--- pages/order_form/clear-button-in-orders.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('click', function (event) {
-   if (event.target.hasAttribute('clear')) {
-      event.target.closest('.order-card').remove()
-      calcCartPrice()
-      toggleCartStatus()
-   }
-})
